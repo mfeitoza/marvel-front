12b338469c8def78f26b0f7708001fd4468c8acf
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2021-06-08 19:34:17 -0300

    feat(components): add footer

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+
+export default function Navbar() {
+  return (
+    <>
+      <footer className="row fixed-bottom mt-2">
+        <div className="col-12 bg-primary text-white p-2 text-center">
+          <strong>Sistema Acadêmico</strong> - Rua dos Avestruzes, nº 51, Porto Canoa. Serra - ES
+          <br />
+          <i class="bi bi-telephone-fill"></i> 27 99914-3696 | 27 3361-4100
+        </div>
+      </footer>
+    </>
+  );
+}

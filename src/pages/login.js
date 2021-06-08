8dbc6ba63a10ef9d0e2fe054c5fa82338876b0fb
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2021-06-08 20:35:00 -0300

    fix(pages): login text

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function login() {
   return (
     <>
       <main class="form-signin text-center">
         <form>
           <i class="bi bi-journal-text fs-1"></i>
-          <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+          <h1 class="h3 mb-3 fw-normal">Sistema Acadêmico</h1>
 
           <div class="form-floating">
             <input
@@ -13,7 +15,7 @@ export default function login() {
               id="floatingInput"
               placeholder="name@example.com"
             />
-            <label for="floatingInput">Email address</label>
+            <label for="floatingInput">Email</label>
           </div>
           <div class="form-floating">
             <input
@@ -22,12 +24,14 @@ export default function login() {
               id="floatingPassword"
               placeholder="Password"
             />
-            <label for="floatingPassword">Password</label>
+            <label for="floatingPassword">Senha</label>
           </div>
 
+          <Link to="/inicio">
           <button class="w-100 btn btn-lg btn-primary" type="submit">
-            Sign in
+            LogIn
           </button>
+          </Link>
           <p class="mt-5 mb-3 text-muted">2021</p>
         </form>
       </main>

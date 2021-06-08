51b24609d3aec4eabefd177080ba26d6bcd79e8c
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2021-06-08 19:57:53 -0300

    feat(pages): add classes

---
+++ src/pages/Classes/createClass.js
@@ -0,0 +1,57 @@
+export default function createClass() {
+  return (
+    <>
+      <div className="container">
+        <h1>Nova Classe</h1>
+        <form>
+          <div class="mt-5 pt-3 form-row">
+            <div class="form-group col-md-6">
+              <label for="inputEmail4">ID</label>
+              <input
+                type="text"
+                class="form-control"
+                id="inputEmail4"
+                placeholder=""
+              />
+            </div>
+            <div class="form-group col-md-6">
+              <label for="inputEmail4">Nome do Classe</label>
+              <input
+                type="text"
+                class="form-control"
+                id="inputEmail4"
+                placeholder=""
+              />
+            </div>
+            <div class="form-group col-md-6">
+              <label for="inputPassword4">Carga Horária</label>
+              <input
+                type="text"
+                class="form-control"
+                id="inputPassword4"
+                placeholder=""
+              />
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="inputAddress">Descrição</label>
+            <input
+              type="text"
+              class="form-control"
+              id="inputAddress"
+              placeholder=""
+            />
+          </div>
+          <div class="form-row"></div>
+          <br />
+          <button type="submit" class="btn btn-primary">
+            Cadastrar
+          </button>{" "}
+          <button type="submit" class="btn btn-primary">
+            Editar
+          </button>
+        </form>
+      </div>
+    </>
+  );
+}

b1502b6789f4c039e295c20d0a17845b112f99b2
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2021-07-01 18:23:41 -0300

    feat(pages): class forms

--- src/pages/classes/createClass.js
+++ src/pages/classes/createClass.js
@@ -1,49 +1,65 @@
-import Layout from "../layout"
+import Layout from "../layout";
+import { useForm } from "react-hook-form";
 
 export default function CreateClass() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
     <>
       <Layout>
         <h1>Nova Classe</h1>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="mt-5 pt-3 form-row">
             <div className="form-group col-md-6">
-              <label for="inputEmail4">ID</label>
+              <label for="courseId">Curso</label>
               <input
                 type="text"
                 className="form-control"
-                id="inputEmail4"
-                placeholder=""
+                {...register("courseId")}
               />
             </div>
             <div className="form-group col-md-6">
-              <label for="inputEmail4">Nome do Classe</label>
+              <label for="name">Vagas</label>
               <input
-                type="text"
+                type="number"
                 className="form-control"
-                id="inputEmail4"
-                placeholder=""
+                {...register("availablePlaces")}
               />
             </div>
             <div className="form-group col-md-6">
-              <label for="inputPassword4">Carga Horária</label>
+              <label for="location">Local</label>
               <input
                 type="text"
                 className="form-control"
-                id="inputPassword4"
-                placeholder=""
+                {...register("location")}
               />
             </div>
           </div>
           <div className="form-group">
-            <label for="inputAddress">Descrição</label>
+            <label for="startDate">Data de Início</label>
             <input
-              type="text"
+              type="date"
               className="form-control"
-              id="inputAddress"
-              placeholder=""
+              {...register("startDate")}
             />
           </div>
+          <div className="form-group">
+            <label for="endDate">Data de Fim</label>
+            <input
+              type="date"
+              className="form-control"
+              {...register("endDate")}
+            />
+          </div>
+          <div className="form-group col-md-6">
+              <label for="professorId">Professor</label>
+              <input
+                type="text"
+                className="form-control"
+                {...register("professorId")}
+              />
+            </div>
           <div className="form-row"></div>
           <br />
           <button type="submit" className="btn btn-primary">

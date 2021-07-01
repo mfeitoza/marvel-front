607327f7b1814360d9fcd13a6439e0803450cc8a
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2021-07-01 01:06:10 -0300

    feat(pages): list students

--- src/pages/students/listStudents.js
+++ src/pages/students/listStudents.js
@@ -1,44 +1,49 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-import Layout from "../layout"
+import Layout from "../layout";
+import { getStudentes } from "../../api";
 
-export default function CistStudents() {
+export default function ListStudents(props) {
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    getStudentes().then((data) => {
+      setStudents(data);
+    });
+  }, []);
+
   return (
     <>
       <Layout>
-        <h1>Estudantes</h1>
+        <h1>Professores</h1>
 
         <table className="table">
           <thead>
             <tr>
               <th scope="col">#</th>
-              <th scope="col">First</th>
-              <th scope="col">Last</th>
-              <th scope="col">Handle</th>
+              <th scope="col">Nome</th>
+              <th scope="col">Email</th>
+              <th scope="col">Telefone</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td colSpan="2">Larry the Bird</td>
-              <td>@twitter</td>
-            </tr>
+            {students.map((student) => (
+              <tr>
+                <th scope="row">{ student.id }</th>
+                <td>{ student.name }</td>
+                <td>{ student.email }</td>
+                <td>{ student.phone }</td>
+              </tr>
+            ))}
           </tbody>
         </table>
 
-        <Link to="/novo-aluno"><a className="btn btn-primary" role="button">Cadastrar</a></Link>
+        <Link to="/novo-professor">
+          <a className="btn btn-primary" role="button">
+            Cadastrar
+          </a>
+        </Link>
       </Layout>
     </>
   );

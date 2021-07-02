66409db5cf7a7f6d64bc1a0d7dedc8cd6f157751
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2021-07-02 16:29:40 -0300

    feat(pages): create student

--- src/pages/students/createStudent.js
+++ src/pages/students/createStudent.js
@@ -1,71 +1,72 @@
-import Layout from "../layout"
+import Layout from "../layout";
+import { useForm, } from "react-hook-form";
+import { useHistory } from "react-router-dom";
+
+import { saveStudentes } from "../../api";
 
 export default function CreateStudent() {
+  let history = useHistory();
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    saveStudentes(data).then(res => {
+      history.push("/alunos");
+    }).catch(() => alert("ERRO! Tente novamente."))
+  }
+
   return (
     <>
       <Layout>
-        <h1>Novo Aluno</h1>
-
-        <form>
+        <h1>Novo Professor</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="mt-5 pt-3 form-row">
             <div className="form-group col-md-6">
-              <label for="inputEmail4">ID</label>
+              <label for="name">Nome</label>
               <input
                 type="text"
                 className="form-control"
-                id="inputEmail4"
-                placeholder=""
+                {...register("name", { required: true })}
               />
             </div>
             <div className="form-group col-md-6">
-              <label for="inputEmail4">Nome</label>
-              <input
-                type="text"
-                className="form-control"
-                id="inputEmail4"
-                placeholder=""
-              />
+              <label for="gender">Genero</label>
+              <select class="form-select" aria-label="Default select example" {...register("gender", { required: true })}>
+                <option selected></option>
+                <option value="male">Masculino</option>
+                <option value="female">Feminino</option>
+              </select>
             </div>
             <div className="form-group col-md-6">
-              <label for="inputPassword4">Gênero</label>
-              <input
-                type="text"
-                className="form-control"
-                id="inputPassword4"
-                placeholder=""
-              />
-            </div>
-            <div className="form-group col-md-6">
-              <label for="inputEmail4">Email</label>
+              <label for="email">Email</label>
               <input
                 type="email"
                 className="form-control"
-                id="inputEmail4"
-                placeholder="Email"
+                {...register("email", { required: true })}
               />
             </div>
           </div>
-          <div className="form-group">
-            <label for="inputAddress">Data de Nascimento</label>
+          <div className="form-group col-md-6">
+            <label for="birthday">Data de Nascimento</label>
             <input
               type="date"
               className="form-control"
-              id="inputAddress"
-              placeholder="1234 Main St"
+              {...register("birthday", { required: true })}
             />
           </div>
           <div className="form-row">
             <div className="form-group col-md-6">
-              <label for="inputCity">telefone</label>
-              <input type="text" className="form-control" id="inputCity" />
+              <label for="phone">Telefone</label>
+              <input type="text" className="form-control" {...register("phone", { required: true })} />
+            </div>
+          </div>
+          <div className="form-row">
+            <div className="form-group col-md-6">
+              <label for="password">Senha</label>
+              <input type="password" className="form-control" {...register("password", { required: true })} />
             </div>
           </div>
           <br />
           <button type="submit" className="btn btn-primary">
             Cadastrar
-          </button>{" "}
-          <button type="submit" className="btn btn-primary">
-            Editar
           </button>
         </form>
       </Layout>

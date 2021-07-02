5babe12a825cfb052feb4e4532aaaf686ec0c311
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2021-07-02 16:30:01 -0300

    feat(api)

--- src/api.js
+++ src/api.js
@@ -28,18 +28,26 @@ export function login(email, password) {
   return api.post('login', { json: { email, password } }).json()
 }
 
-export function saveClasses() {
-
+export function saveClasses({ location, availablePlaces, startDate, endDate, courseId, professorId }) {
+  return api.post('classes', { json: {
+    location, availablePlaces, startDate, endDate, courseId, professorId
+  }})
 }
 
-export function saveCourses() {
-
+export function saveCourses({ name, workload, description }) {
+  return api.post('courses', { json: {
+    name, workload, description
+  }})
 }
 
-export function saveProfessors() {
-
+export function saveProfessors({ name, email, password, phone, gender, birthday, role = "professor" }) {
+  return api.post('professors', { json: {
+    name, email, password, phone, gender, birthday, role
+  }})
 }
 
-export function saveStudentes() {
-  
+export function saveStudentes({ name, email, password, phone, gender, birthday, role = "student" }) {
+  return api.post('students', { json: {
+    name, email, password, phone, gender, birthday, role
+  }})
 }
